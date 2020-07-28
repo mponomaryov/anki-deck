97308ef937774ab7c650c1b101ff62477eccb344
Mihail Ponomaryov <mihail.ponomaryov@gmail.com>
2020-07-28 19:29:24 +0300

    verbs

---
+++ verbs.py
@@ -0,0 +1,73 @@
+import genanki
+
+
+DECK_ID = 7469112283
+MODULE_ID = 2417888824
+
+CSS = '''
+.preterite::before,
+.partizip2::before {
+    color: gray;
+    font-size: 70%;
+    font-weight: normal;
+    font-style: italic;
+}
+
+.preterite::before {
+    content: "Preterite: ";
+}
+
+.partizip2::before {
+    content: "Partizip 2: ";
+}
+'''
+
+Model = genanki.Model(
+    MODULE_ID,
+    fields = [
+        {'name': 'Infinitive'},
+        {'name': 'Preterite'},
+        {'name': 'Partizip2'},
+        {'name': 'Translation'},
+    ],
+    css=CSS,
+    templates=[
+        {
+            'name': 'Card 1',
+            'qfmt': (
+                '{{Infinitive}}<br>'
+                '<span class="preterite">{{Preterite}}</span><br>'
+                '<span class="partizip2">{{Partizip2}}</span>'
+            ),
+            'afmt': '{{FrontSide}}<hr id="answer">{{Translation}}'
+        },
+        {
+            'name': 'Card 2',
+            'qfmt': '{{Translation}}',
+            'afmt': (
+                '{{FrontSide}}<hr id="answer">'
+                '{{Infinitive}}<br>'
+                '<span class="preterite">{{Preterite}}</span><br>'
+                '<span class="partizip2">{{Partizip2}}</span>'
+            ),
+        },
+    ]
+)
+
+deck = genanki.Deck(DECK_ID, 'German Verbs')
+
+with open('german_verbs.txt') as file:
+    for line in file:
+        infinitive, translation, preterite, partizip2 = line.strip().split('|')
+
+        if infinitive.endswith('*'):
+            infinitive = f'<span style="color: red">{infinitive[:-1]}</span>'
+
+        deck.add_note(
+            genanki.Note(
+                model=Model,
+                fields=(infinitive, preterite, partizip2, translation)
+            )
+        )
+
+genanki.Package(deck).write_to_file('GermanVerbs.apkg')
